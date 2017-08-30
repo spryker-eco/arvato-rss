e85f6cab273bfdf26b1ef1ebbf77789da0899458
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-08-30 15:57:37 +0300

    ECO-706. Request header converter.

---
+++ src/SprykerEco/Zed/ArvatoRss/Business/Api/Converter/RiskCheckRequestToHeaderConverter.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\ArvatoRss\Business\Api\Converter;
+
+use Generated\Shared\Transfer\ArvatoRssIdentificationRequestTransfer;
+use Generated\Shared\Transfer\ArvatoRssRiskCheckRequestTransfer;
+
+class RiskCheckRequestToHeaderConverter implements RiskCheckRequestToHeaderConverterInterface
+{
+
+    /**
+     * @const string IDENTIFICATION_NAMESPACE
+     */
+    const IDENTIFICATION_NAMESPACE = 'urn:risk-solution-services-types:v2.1';
+
+    /**
+     * @const string IDENTIFICATION_HEADER_NAME
+     */
+    const IDENTIFICATION_HEADER_NAME = 'Identification';
+
+    /**
+     * @param \Generated\Shared\Transfer\ArvatoRssRiskCheckRequestTransfer $arvatoRssRiskCheckRequestTransfer
+     *
+     * @return \SoapHeader
+     */
+    public function convert(ArvatoRssRiskCheckRequestTransfer $arvatoRssRiskCheckRequestTransfer)
+    {
+        $identification = $arvatoRssRiskCheckRequestTransfer->getIdentification();
+        $requestData = $this->createRequestData($identification);
+        $soapHeader = new \SoapHeader(
+            self::IDENTIFICATION_NAMESPACE,
+            self::IDENTIFICATION_HEADER_NAME,
+            $requestData
+        );
+
+        return $soapHeader;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ArvatoRssIdentificationRequestTransfer $arvatoRssIdentificationRequestTransfer
+     *
+     * @return array
+     */
+    protected function createRequestData(ArvatoRssIdentificationRequestTransfer $arvatoRssIdentificationRequestTransfer)
+    {
+        $result = [];
+        $fields = $this->getRequestFields();
+        array_walk($fields, function ($item, $key) use (
+            &$result, &$arvatoRssIdentificationRequestTransfer
+        ) {
+            $result[$key] = $arvatoRssIdentificationRequestTransfer->{'get'.$item}();
+        });
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getRequestFields()
+    {
+        return [
+            'ClientID' => 'clientId',
+            'Authorisation' => 'authorisation'
+        ];
+    }
+
+}
