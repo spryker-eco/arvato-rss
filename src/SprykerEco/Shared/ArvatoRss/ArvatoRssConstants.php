2cfd09845b3ed8edd0f7102009cfcbcd9cc7b3e3
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-08-30 16:29:49 +0300

    ECO-706. Config updated.

--- src/SprykerEco/Shared/ArvatoRss/ArvatoRssConstants.php
+++ src/SprykerEco/Shared/ArvatoRss/ArvatoRssConstants.php
@@ -6,9 +6,9 @@ interface ArvatoRssConstants
 {
 
     const ARVATORSS = "ARVATORSS";
-
+    const ARVATORSS_URL = 'ARVATORSS_URL';
     // Authorization data
-    const ARVATORSS_CLIENTID = "CLIENTID";
-    const ARVATORSS_PASSWORD = "PASSWORD";
+    const ARVATORSS_CLIENTID = "ARVATORSS_CLIENTID";
+    const ARVATORSS_PASSWORD = "ARVATORSS_PASSWORD";
 
 }
