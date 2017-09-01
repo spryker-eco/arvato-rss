bd904e15f69b2989c333ac958966e2ec9fed9cca
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-09-01 15:37:09 +0000

    ECO-706. Test mock files and mapper tests.

---
+++ tests/Unit/SprykerEco/Zed/ArvatoRss/Business/Api/Mapper/RiskCheckRequestMapperTest.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Unit\SprykerEco\Zed\ArvatoRss\Business\Api\Mapper;
+
+use Generated\Shared\Transfer\AddressTransfer;
+use Generated\Shared\Transfer\ArvatoRssBillingCustomerTransfer;
+use Generated\Shared\Transfer\ArvatoRssCustomerAddressTransfer;
+use Generated\Shared\Transfer\ArvatoRssIdentificationRequestTransfer;
+use Generated\Shared\Transfer\ArvatoRssOrderItemTransfer;
+use Generated\Shared\Transfer\ArvatoRssOrderTransfer;
+use Generated\Shared\Transfer\ArvatoRssRiskCheckRequestTransfer;
+use Generated\Shared\Transfer\CustomerTransfer;
+use Generated\Shared\Transfer\ItemTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
+use PHPUnit\Framework\TestCase;
+use SprykerEco\Zed\ArvatoRss\Business\Api\Mapper\RiskCheckRequestMapper;
+use SprykerEco\Zed\ArvatoRss\Dependency\Facade\ArvatoRssToMoneyBridge;
+
+class RiskCheckRequestMapperTest extends TestCase
+{
+
+    const INT_VALUE = 12475;
+    const DECIMAL_VALUE = 124.75;
+
+    /**
+     * @dataProvider userData
+     */
+    public function testMapQuoteToRequestTranfer($data)
+    {
+        $mapper = $this->createMapper();
+
+        $quoteTranfer = $this->getQuoteTranfer($data);
+        $expected = $this->getExpectedRequestTransfer($data)->toArray(true);
+        $actual = $mapper->mapQuoteToRequest($quoteTranfer)->toArray(true);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    public function userDataProvider()
+    {
+        return [
+            [
+                new \ArrayObject(
+                    [
+                        'clientId'          => '00000000',
+                        'authorisation'     => '11111111',
+                        'country'           => 'DE',
+                        'city'              => 'Berlin',
+                        'street'            => 'Europa-Allee 50',
+                        'zipCode'           => '60327',
+                        'firstName'         => 'Michael',
+                        'lastName'          => 'Duglas',
+                        'salutation'        => 'Mr.',
+                        'email'             => 'duglas@gmail.com',
+                        'telephoneNumber'   => '123213',
+                        'birthDay'          => '20/04/1978',
+                        'position'          => 1,
+                        'productNumber'     => '777777',
+                        'unitPrice'         => static::INT_VALUE,
+                        'unitCount'         => 1
+                    ]
+                )
+            ]
+        ];
+    }
+
+    /**
+     * @param \ArrayObject $data
+     *
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    protected function getQuoteTranfer($data)
+    {
+        $quoteTransfer = new QuoteTransfer();
+        $customerTranfer = new CustomerTransfer();
+        $billingAddress = new AddressTransfer();
+        $orderItem = new ItemTransfer();
+
+        $billingAddress->setIso2Code($data->getCountry());
+        $billingAddress->setCity($data->getCity());
+        $billingAddress->setAddress1($data->getStreet());
+        $billingAddress->setZipCode($data->getZipCode());
+
+        $customerTranfer->setFirstName($data->getFirstName());
+        $customerTranfer->setLastName($data->getLastName());
+        $customerTranfer->setSalutation($data->getSalutation());
+        $customerTranfer->setEmail($data->getEmail());
+        $customerTranfer->setPhone($data->getPhoneNumber());
+        $customerTranfer->setDateOfBirth($data->getBirthDay());
+
+        $orderItem->setUnitPrice($data->getUnitPrice());
+        $orderItem->setSku($data->getProductNumber());
+
+        $quoteTransfer->setCustomer($customerTranfer);
+        $quoteTransfer->setBillingAddress($billingAddress);
+        $quoteTransfer->addItem($orderItem);
+
+        return $quoteTransfer;
+    }
+
+    /**
+     * @param \ArrayObject $data
+     *
+     * @return \Generated\Shared\Transfer\ArvatoRssRiskCheckRequestTransfer
+     */
+    protected function getExpectedRequestTransfer($data)
+    {
+        $requestTransfer = new ArvatoRssRiskCheckRequestTransfer();
+        $identificationTransfer = new ArvatoRssIdentificationRequestTransfer();
+        $billingCustomerTransfer = new ArvatoRssBillingCustomerTransfer();
+        $address = new ArvatoRssCustomerAddressTransfer();
+        $orderTransfer = new ArvatoRssOrderTransfer();
+        $itemTransfer = new ArvatoRssOrderItemTransfer();
+
+        $identificationTransfer->setClientId($data->getClientId());
+        $identificationTransfer->setAuthorisation($data->getAuthorisation());
+
+        $address->setCountry($data->getCountry());
+        $address->setCity($data->getCity());
+        $address->setStreet($data->getStreet());
+        $address->setZipCode($data->getZipCode());
+        $billingCustomerTransfer->setAddress($address);
+        $billingCustomerTransfer->setFirstName($data->getFirstName());
+        $billingCustomerTransfer->setLastName($data->getLastName());
+        $billingCustomerTransfer->setSalutation($data->getSalutation());
+        $billingCustomerTransfer->setEmail($data->getEmail());
+        $billingCustomerTransfer->setTelephoneNumber($data->getTelephoneNumber());
+        $billingCustomerTransfer->setBirthDay($data->getBirthDay());
+
+        $itemTransfer->setPosition($data->getPosition());
+        $itemTransfer->setProductNumber($data->getProductNumber());
+        $itemTransfer->setUnitPrice(static::INT_VALUE);
+        $itemTransfer->setUnitCount(1);
+
+        $orderTransfer->addItem($itemTransfer);
+        $requestTransfer->setIdentification($identificationTransfer);
+        $requestTransfer->setBillingCustomer($billingCustomerTransfer);
+        $requestTransfer->setOrder($orderTransfer);
+
+        return $requestTransfer;
+    }
+
+    /**
+     * @return \SprykerEco\Zed\ArvatoRss\Business\Api\Mapper\RiskCheckRequestMapper
+     */
+    protected function createMapper()
+    {
+        return new RiskCheckRequestMapper($this->createMoneyFacadeMock());
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function createMoneyFacadeMock()
+    {
+        $moneyFacadeMock = $this->createPartialMock(
+            ArvatoRssToMoneyBridge::class,
+            ['convertIntegerToDecimal']
+        );
+        $moneyFacadeMock->method('convertIntegerToDecimal')
+            ->willReturn(static::DECIMAL_VALUE);
+
+        return $moneyFacadeMock;
+    }
+
+}
---
+++ tests/Unit/SprykerEco/Zed/ArvatoRss/Business/Api/Mapper/RiskCheckResponseMapperTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Unit\SprykerEco\Zed\ArvatoRss\Business\Api\Mapper;
+
+use Generated\Shared\Transfer\ArvatoRssRiskCheckResponseTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
+use PHPUnit\Framework\TestCase;
+use SprykerEco\Zed\ArvatoRss\Business\Api\Mapper\RiskCheckResponseMapper;
+use \ArrayObject;
+
+//TODO: please, configure project to run tests and check tests.
+class RiskCheckResponseMapperTest extends TestCase
+{
+
+    /**
+     * @dataProvider responseDataProvider
+     */
+    public function mapResponseToQuoteTest($data)
+    {
+        $mapper = $this->createMapper();
+
+        $expected = $this->createExpectedTransfer($data)->toArray(true);
+        $actual = $mapper->mapResponseToQuote(
+            $this->createResponseTransfer($data),
+            $this->createQuoteTranfer()
+        )->toArray(true);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    public function responseDataProvider()
+    {
+        return [
+            [
+                new \ArrayObject(
+                    [
+                        'result' => 'R',
+                        'resultCode' => 'R',
+                        'actionCode' => 'IPT999',
+                        'resultDescription' => 'Test',
+                        'scoreValue' => '12',
+                        'scoreFeatureName' => 'Test',
+                        'scoreFeatuteValue' => 'Test',
+                        'rmfCustomerId' => '123',
+                        'isNewCustomer' => true,
+                        'communicationToken' => '123123',
+                        'returnCode' => '0',
+                        'street' => 'Europa-Allee 50',
+                        'streetNumber' => '12',
+                        'zipCode' => '12312',
+                        'city' => 'Berlin'
+                    ]
+                )
+            ]
+        ];
+    }
+
+    /**
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    protected function createQuoteTranfer()
+    {
+        return new QuoteTransfer();
+    }
+
+    /**
+     * @param \ArrayObject $data
+     *
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    protected function createExpectedTransfer(ArrayObject $data)
+    {
+        $quoteTranfer = $this->createQuoteTranfer();
+        $responseTransfer = $this->createResponseTransfer($data);
+        $quoteTranfer->setArvatoRssRiskCheckResponse($responseTransfer);
+
+        return $quoteTranfer;
+    }
+
+    /**
+     * @param \ArrayObject $data
+     *
+     * @return \Generated\Shared\Transfer\ArvatoRssRiskCheckResponseTransfer
+     */
+    protected function createResponseTransfer(ArrayObject $data)
+    {
+        $transfer = new ArvatoRssRiskCheckResponseTransfer();
+        $transfer->setResult($data->getResult());
+        $transfer->setActionCode($data->getActionCode());
+        $transfer->setResultCode($data->getResultCode());
+        $transfer->setResultDescription($data->getResultDescription());
+        $transfer->setScoreValue($data->getScoreValue());
+        $transfer->setScoreFeatureName($data->getScoreFeatureName());
+        $transfer->setScoreFeatureValue($data->getScoreFeatureValue());
+        $transfer->setRmfCustomerId($data->getRmfCustomerId());
+        $transfer->setIsNewCustomer($data->getIsNewCustomer());
+        $transfer->setCommunicationToken($data->getCommunicationToken());
+        $transfer->setReturnCode($data->getReturnCode());
+        $transfer->setStreet($data->getStreet());
+        $transfer->setStreetNumber($data->getStreetNumber());
+        $transfer->setZipCode($data->getZipCode());
+        $transfer->setCity($data->getCity());
+
+        return $transfer;
+    }
+
+    /**
+     * @return \SprykerEco\Zed\ArvatoRss\Business\Api\Mapper\RiskCheckResponseMapper
+     */
+    protected function createMapper()
+    {
+        return new RiskCheckResponseMapper();
+    }
+
+}
