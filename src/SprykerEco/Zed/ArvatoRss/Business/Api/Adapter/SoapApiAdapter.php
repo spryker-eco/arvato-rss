b04cda649c1ffa15154539a5550c46aeb0be4bd6
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-09-21 09:43:12 +0000

    ECO-706. Add simple error handling.

--- src/SprykerEco/Zed/ArvatoRss/Business/Api/Adapter/SoapApiAdapter.php
+++ src/SprykerEco/Zed/ArvatoRss/Business/Api/Adapter/SoapApiAdapter.php
@@ -8,9 +8,12 @@
 namespace SprykerEco\Zed\ArvatoRss\Business\Api\Adapter;
 
 use Generated\Shared\Transfer\ArvatoRssRiskCheckRequestTransfer;
+use Spryker\Shared\Config\Config;
+use SprykerEco\Shared\ArvatoRss\ArvatoRssConstants;
 use SprykerEco\Zed\ArvatoRss\Business\Api\Converter\RiskCheckRequestConverter;
 use SprykerEco\Zed\ArvatoRss\Business\Api\Converter\RiskCheckRequestHeaderConverter;
 use SprykerEco\Zed\ArvatoRss\Business\Api\Converter\RiskCheckResponseConverter;
+use SprykerEco\Zed\ArvatoRss\Business\Api\Exception\ArvatoRssRiskCheckApiException;
 
 class SoapApiAdapter implements ApiAdapterInterface
 {
@@ -60,10 +63,22 @@ class SoapApiAdapter implements ApiAdapterInterface
     {
         $params = $this->riskCheckRequestConverter->convert($requestTransfer);
         $header = $this->riskCheckRequestHeaderConverter->convert($requestTransfer);
-        $options = [];
+        $options = [
+            'exceptions' => false
+        ];
         $soapClient = new \SoapClient(static::WSDL_PATH, $options);
         $soapClient->__setSoapHeaders($header);
+        // TODO: add config get service to avoid direct config call
+        $soapClient->__setLocation(
+            Config::get(ArvatoRssConstants::ARVATORSS)[ArvatoRssConstants::ARVATORSS_URL]
+        );
+
         $result = $soapClient->RiskCheck($params);
+        if ($result instanceof \SoapFault) {
+            $exceptionName = array_keys(get_object_vars($result->detail))[0];
+            $exceptionObj = $result->detail->{$exceptionName};
+            throw new ArvatoRssRiskCheckApiException($exceptionObj->Description);
+        }
 
         return $this->riskCheckResponseConverter->convert($result);
     }
