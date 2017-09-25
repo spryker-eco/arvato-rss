3b1df4ad840c4a1b775349f3e8ebcf707d40df7f
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-09-25 10:14:53 +0000

    ECO-706. Update return types in interface.

--- src/SprykerEco/Service/ArvatoRss/Iso3166ConverterInterface.php
+++ src/SprykerEco/Service/ArvatoRss/Iso3166ConverterInterface.php
@@ -13,14 +13,14 @@ interface Iso3166ConverterInterface
     /**
      * @param string $iso2CountryCode
      *
-     * @return int
+     * @return string|null
      */
     public function iso2ToNumeric($iso2CountryCode);
 
     /**
      * @param int $iso3166CountryCode
      *
-     * @return string
+     * @return string|null
      */
     public function numericToIso2($iso3166CountryCode);
 
