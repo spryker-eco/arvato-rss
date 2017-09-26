3dc46cee7715772d481ffcc53277f5b333d59a26
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-09-26 08:18:24 +0000

    ECO-706. Add street number constant.

--- src/SprykerEco/Zed/ArvatoRss/Business/Api/ArvatoRssApiConstants.php
+++ src/SprykerEco/Zed/ArvatoRss/Business/Api/ArvatoRssApiConstants.php
@@ -9,6 +9,7 @@ interface ArvatoRssApiConstants
     const ARVATORSS_API_COUNTRY = 'Country';
     const ARVATORSS_API_CITY = 'City';
     const ARVATORSS_API_STREET = 'Street';
+    const ARVATORSS_API_STREET_NUMBER = 'StreetNumber';
     const ARVATORSS_API_ZIPCODE = 'ZipCode';
     const ARVATORSS_API_BILLINGCUSTOMER = 'BillingCustomer';
     const ARVATORSS_API_FIRSTNAME = 'FirstName';
