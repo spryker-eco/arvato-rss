3b3d33445bded5149393205ce8b5b594e84f85b0
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-10-09 12:53:21 +0300

    Move API urls from config.dist.php to documentation.

--- config/config.dist.php
+++ config/config.dist.php
@@ -2,7 +2,6 @@
 
 use SprykerEco\Shared\ArvatoRss\ArvatoRssConstants;
 
-// Production url: https://customer.risk-solution-services.de/rss-services/risk-solution-services.v2.1
-$config[ArvatoRssConstants::ARVATORSS_URL] = 'https://integration.risk-solution-services.de/rss-services/risk-solution-services.v2.1';
+$config[ArvatoRssConstants::ARVATORSS_URL] = '';
 $config[ArvatoRssConstants::ARVATORSS_CLIENTID] = '';
 $config[ArvatoRssConstants::ARVATORSS_PASSWORD] = '';
