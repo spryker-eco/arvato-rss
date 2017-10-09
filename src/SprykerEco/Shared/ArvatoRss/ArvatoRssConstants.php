3b7b66ee9cd9602c2396f74af3c2babd30919559
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-10-09 13:38:46 +0300

    Remove comment from constants class.

--- src/SprykerEco/Shared/ArvatoRss/ArvatoRssConstants.php
+++ src/SprykerEco/Shared/ArvatoRss/ArvatoRssConstants.php
@@ -7,7 +7,6 @@ interface ArvatoRssConstants
 
     const ARVATORSS = "ARVATORSS";
 
-    // Authorization data
     const ARVATORSS_URL = "ARVATORSS_URL";
     const ARVATORSS_CLIENTID = "CLIENTID";
     const ARVATORSS_PASSWORD = "PASSWORD";
