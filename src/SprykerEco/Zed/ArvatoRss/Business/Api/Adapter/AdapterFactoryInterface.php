1b3bd62a031c32fa5ec06444103e739349e74d03
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-11-08 15:12:32 +0000

    ECO-967. Add return types to interface methods.

--- src/SprykerEco/Zed/ArvatoRss/Business/Api/Adapter/AdapterFactoryInterface.php
+++ src/SprykerEco/Zed/ArvatoRss/Business/Api/Adapter/AdapterFactoryInterface.php
@@ -11,13 +11,28 @@ namespace SprykerEco\Zed\ArvatoRss\Business\Api\Adapter;
 
 interface AdapterFactoryInterface
 {
+    /**
+     * @return \SprykerEco\Zed\ArvatoRss\Business\Api\Converter\RequestHeaderConverter
+     */
     public function createRequestHeaderConverter();
 
+    /**
+     * @return \SprykerEco\Zed\ArvatoRss\Business\Api\Converter\RiskCheckRequestConverterInterface
+     */
     public function createRiskCheckRequestConverter();
 
+    /**
+     * @return \SprykerEco\Zed\ArvatoRss\Business\Api\Converter\RiskCheckRequestConverterInterface
+     */
     public function createRiskCheckResponeConverter();
 
+    /**
+     * @return \SprykerEco\Zed\ArvatoRss\Business\Api\Converter\RiskCheckRequestConverterInterface
+     */
     public function createStoreOrderRequestConverter();
 
+    /**
+     * @return \SprykerEco\Zed\ArvatoRss\Business\Api\Converter\StoreOrderResponseConverterInterface
+     */
     public function createStoreOrderResponseConverter();
 }
