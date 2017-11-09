37105829ffd99926b933ad66a7d9ec0abde9ded6
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-11-09 10:46:26 +0000

    ECO-967. Add needed getters to module config.

--- src/SprykerEco/Zed/ArvatoRss/ArvatoRssConfig.php
+++ src/SprykerEco/Zed/ArvatoRss/ArvatoRssConfig.php
@@ -8,7 +8,23 @@
 namespace SprykerEco\Zed\ArvatoRss;
 
 use Spryker\Zed\Kernel\AbstractBundleConfig;
+use SprykerEco\Shared\ArvatoRss\ArvatoRssConstants;
 
 class ArvatoRssConfig extends AbstractBundleConfig
 {
+    /**
+     * @return string
+     */
+    public function getClientId()
+    {
+        return $this->get(ArvatoRssConstants::ARVATORSS_CLIENTID);
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorization()
+    {
+        return $this->get(ArvatoRssConstants::ARVATORSS_AUTHORISATION);
+    }
 }
