d44d79e1f85db7adeb26704e445eae85b3f88762
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-11-14 12:03:49 +0000

    ECO-967. Extend store order response converter.

--- src/SprykerEco/Zed/ArvatoRss/Business/Api/Converter/StoreOrderResponseConverter.php
+++ src/SprykerEco/Zed/ArvatoRss/Business/Api/Converter/StoreOrderResponseConverter.php
@@ -17,8 +17,17 @@ class StoreOrderResponseConverter implements StoreOrderResponseConverterInterfac
      *
      * @return \Generated\Shared\Transfer\ArvatoRssStoreOrderResponseTransfer
      */
-    public function convert(stdClass $response)
+    public function convert(stdClass $outcome)
     {
-        return new ArvatoRssStoreOrderResponseTransfer();
+        $response = new ArvatoRssStoreOrderResponseTransfer();
+        $response->setTransactionId($outcome->System->TransactionId)
+            ->setStatusCode($outcome->System->StatusCode)
+            ->setResult($outcome->Decision->Result)
+            ->setActionCode($outcome->Decision->ActionCode)
+            ->setResultCode($outcome->Decision->ResultCode)
+            ->setResultText($outcome->Decision->ResultText)
+            ->setIsNewCustomer($outcome->Decision->IsNewCustomer);
+
+        return $response;
     }
 }
